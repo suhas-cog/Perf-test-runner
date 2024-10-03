7f08422ac8c1d731b039023599885f759c0c23fa
G <2302372@cognizant.com>
2024-10-03 21:09:10 +0530

    Latest-delete

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Execution is being started"
-echo "**************************"
-jmeter $@
-echo "**************************"
-echo "Execution has been completed, please check the artifacts to download the results."
